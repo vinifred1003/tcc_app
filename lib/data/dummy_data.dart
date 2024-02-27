55a77dd940cb35468878a81f51303672bd8b52af
vinifred1003 <viniciusfrederico1003@gmail.com>
2024-02-27 11:21:53 -0300

    begining

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,9 @@
+import '../models/user.dart';
+
+const dummyUser = [
+  User(
+    id: 'u1',
+    username: 'vini@gmail.com',
+    password: '123456',
+  )
+];
---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class User {
+  final String id;
+  final String username;
+  final String password;
+
+  const User({
+    required this.id,
+    required this.username,
+    required this.password,
+  });
+}
