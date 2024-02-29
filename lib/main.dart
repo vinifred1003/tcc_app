ca47ca487fb35df045273ad413257fc5f4a15578
vinifred1003 <viniciusfrederico1003@gmail.com>
2024-02-29 11:23:07 -0300

    login screen widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+import './screens/login_screen.dart';
 
 void main() {
   runApp(MeproviApp());
@@ -12,70 +13,35 @@ class MeproviApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData tema = ThemeData();
+
     return MaterialApp(
-      title: 'MeproviAPP',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Bem vindo"),
-        backgroundColor: Colors.lightBlue,
-        actions: const <Widget>[
-          CircleAvatar(
-            radius: 20,
-            child: Padding(
-              padding: EdgeInsets.all(4.0),
-              child: Icon(Icons.question_mark),
-            ),
+      home: LoginScreen(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.lightBlue,
+          secondary: Color.fromARGB(255, 162, 222, 250),
+        ),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: null,
-              child: Text(
-                "Entrar",
-                style: TextStyle(fontSize: 40),
-              ),
-            ),
-          ],
+          labelLarge: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          color: Theme.of(context).colorScheme.primary,
+          titleTextStyle: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      appBar: AppBar(
+        title: Text("Bem vindo"),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: const <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 20,
+            child: Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Icon(Icons.question_mark),
+            ),
+          ),
+        ],
+      ),
+      body: SizedBox(
+        height: 2000,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 90,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '',
+                        labelText: 'E-mail',
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 120,
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Senha',
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Checkbox(
+                              checkColor: Colors.white,
+                              value: isChecked,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  isChecked = value!;
+                                });
+                              },
+                            ),
+                            Text(
+                              'Lembrar-se de mim',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 120,
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Theme.of(context).textTheme.labelLarge?.color,
+                      foregroundColor: Theme.of(context).colorScheme.secondary,
+                      minimumSize: Size(200, 70),
+                    ),
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Esqueci minha senha",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.blue, // Set the color you want
+                        decorationThickness: 2.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: Column(
+                children: [
+                  const Text(
+                    'Não tem cadastro?',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Cadastrar",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.blue, // Set the color you want
+                        decorationThickness: 2.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
