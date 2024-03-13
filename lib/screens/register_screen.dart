799f42602a1dca96bbd60861dc3c09354e522f03
vinifred1003 <viniciusfrederico1003@gmail.com>
2024-03-12 23:32:39 -0300

    ...

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -19,18 +19,84 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: Form(
         child: ListView(
           children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(100),
+                      onTap: () {},
+                      child: Opacity(
+                        opacity: 0.85,
+                        child: SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: Image(
+                            image: AssetImage('assets/images/foto_perfil.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
               child: TextField(
                 decoration: InputDecoration(
                   fillColor: Colors.white,
+                  filled: true,
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(30))),
                   hintText: '',
-                  labelText: 'E-mail',
+                  labelText: 'Nome Completo',
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  hintText: '',
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  hintText: '',
+                  labelText: 'Senha',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  hintText: '',
+                  labelText: 'Confirmar senha',
+                ),
+              ),
+            ),
           ],
         ),
       ),
